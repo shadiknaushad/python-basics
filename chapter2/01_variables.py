20a0b1ef408657fd1a793ec0878ff4fc4ac91627
shadiknaushad <ms1819050@gmail.com>
2026-05-19 16:30:30 +0530

    chapter2

---
+++ chapter2/01_variables.py
@@ -0,0 +1,14 @@
+# variables are container to store some data
+
+name = "shadik"  # identifies name as class <str>
+age = 21   # identifies a as class <int>
+print(name)
+print(age) # identifies a as class <int>
+
+a = 12 
+name = "shadik"  # identifies name as class <str>
+b = 33.33  # identifies b as class <float>
+
+print(a)
+print(name)
+print(b)
---
+++ chapter2/03_operators.py
@@ -0,0 +1,44 @@
+# Arithmatic operators
+a = 7
+b = 4
+c = (a+b)
+print(c)
+
+#assignment operators
+a = 4-2 #assign 4-2 in a
+print(a)
+b = 6
+#b+= 3 # Increment the value of b by 3 and then assign it to b
+b-=3 # Decrement the value of b by 3 and then assign it to b
+
+print(b)
+
+# comparison operators
+f = 4==4
+print(f)
+
+# m = 34
+# n = 46
+# d = m>n
+# print(d)
+
+# m = 43
+# n = 43
+# print(m>=n)
+
+# Logical Operators
+e = True or False
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and' 
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(True))
