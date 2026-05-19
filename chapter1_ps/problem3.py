fbac0552cae34e5be4e26fec25d7aa05c4b6264b
shadiknaushad <ms1819050@gmail.com>
2026-05-19 16:09:15 +0530

    chapter1 problems

---
+++ chapter1_ps/problem3.py
@@ -0,0 +1,25 @@
+'''some mathematical use in python'''
+
+# sum of two number
+a = 12
+b = 15
+print(a+b)
+
+
+# difference of two number
+a = 12
+b = 15
+print(a-b)
+
+
+# multiplication of two number
+a = 12
+b = 15
+print(a*b)
+
+
+# division of two number
+a = 12
+b = 15
+print(a/b)
+
