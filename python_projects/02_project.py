177d25f28af15f7bc81603ad022ecde8d2ef1b85
shadiknaushad <ms1819050@gmail.com>
2026-07-02 22:27:47 +0530

    project

---
+++ python_projects/02_project.py
@@ -0,0 +1,25 @@
+# python calculator
+
+operator = input("Enter the operator(+, -, *, /, %): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+elif operator == "%":
+    result = num1 % num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not valid operator")
+
+
---
+++ python_projects/03_project.py
@@ -0,0 +1,15 @@
+# python weight converter
+
+weight = float(input("Enter your weight: "))
+unit = input("Kilogram or Pounds? (K or L): ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "Lbs."
+    print(f"your weight is: {round(weight, 1)} {unit}")
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "Kgs."
+    print(f"your weight is: {round(weight, 1)} {unit}")
+else:
+    print(f"{unit} is not valid")
