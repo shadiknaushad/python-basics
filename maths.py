650ce3f78bc688f2138ee3c97f18b42f7a5b8fbe
shadiknaushad <ms1819050@gmail.com>
2026-07-02 21:22:24 +0530

    operators and maths

---
+++ maths.py
@@ -0,0 +1,32 @@
+# Math things
+
+
+# x = 4
+# y = 3.4
+# z = 5
+
+# result1 = round(x)
+# result2 = round(y)
+# result3 = round(z)
+# print(result2)
+# result = abs(y)
+# print(result)
+# result = pow(4,3)
+# result = max(x,y,z)
+# result = min(x,y,z)
+# print(result)
+
+
+import math
+# print(math.pi)
+# print(math.e)
+
+# x = 9
+# result = math.sqrt(x)
+
+# x = 9.1
+# result = math.ceil(x)
+# print(result)
+
+
+
---
+++ operators.py
@@ -0,0 +1,16 @@
+a = 5;
+b = 5;
+
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number"))
+
+print(a+b);
+print(a-b);
+print(a*b);
+print(a/b);
+print(a%b);
+
+
+
+
+
