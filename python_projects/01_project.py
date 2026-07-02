bfd445a869990c25a9ed93d0d2b91854d88a1135
shadiknaushad <ms1819050@gmail.com>
2026-07-02 14:11:19 +0530

    third commit

---
+++ python_projects/01_project.py
@@ -0,0 +1,19 @@
+# Gambit Game
+# Words Game Where You Create Story By Filling In Blanks With Random Words.
+
+# hero_name = input("Enter your favorite superhero name: ")
+# superpower = input("Enter any dangerous attack or any superpower: ")
+# enemy = input("Enter enemy name: ")
+# game_item = input("one gaming item(e.g, Medkit, AWM, Katana): ")
+# victory_cry = input("What you scream when you win: ")
+
+# print(f"\n----Your Gamer Story______")
+# print(f"Suddenly, {hero_name} makes a grand entry onto the battleground!")
+# print(f"just like Saitama, they unleashed their deadly {superpower} move.")
+# print(f"The enemy, {enemy}, collapsed instantly without even getting a chance to use a {game_item}!")
+# print(f"The entire squad cheered in excitement: '{victory_cry}!!'")
+
+
+
+
+
