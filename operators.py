2e0c7e7cfc874aaa2946ba05767fd2d0bc96d1a3
shadiknaushad <ms1819050@gmail.com>
2026-07-04 21:15:59 +0530

    commit

--- operators.py
+++ operators.py
@@ -1,14 +1,47 @@
+# Arithmatic operators
+
 a = 5;
 b = 5;
 
 a = int(input("Enter your first number: "))
 b = int(input("Enter your second number"))
-
 print(a+b);
 print(a-b);
 print(a*b);
 print(a/b);
 print(a%b);
+
+# Assignment operator
+
+a = 10
+# a+=5
+# a-=5
+a*=5
+a/=5
+a%=5
+print(a)
+
+# Comparison operator
+
+a = 10
+b = 20
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+
+# Logical operator
+
+age = 21
+print(age > 18 and age < 30)
+print(age > 18 or age > 50)
+print(not(age > 18))
+
+
+
+
+
+
 
 
 
