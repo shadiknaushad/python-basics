430d8e490b47d3c33428202bca04aba4377dea1c
shadiknaushad <ms1819050@gmail.com>
2026-07-07 21:03:52 +0530

    countdown project

---
+++ python_projects/07_projects.py
@@ -0,0 +1,13 @@
+# countdown timer project in python
+
+import time
+
+my_time = int(input("Enter the time in second: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x /60) % 60
+    hour = int(x / 3600)
+    print(f"{hour:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+print("TIME'S UP!")
